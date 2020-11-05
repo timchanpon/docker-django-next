cce983c437292143cb6609c8136e71ceee84918b
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-11-05 15:26:22 +0900

    Introduce nginx

--- django/config/settings.py
+++ django/config/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+	'127.0.0.1',
+	'localhost',
+]
 
 CORS_ALLOWED_ORIGINS = [
 	'http://127.0.0.1:3000',
