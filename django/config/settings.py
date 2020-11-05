aeaf3d1f98dc60113190a19fb299c689549ac126
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-11-05 18:25:55 +0900

    Register static dir for django app

--- django/config/settings.py
+++ django/config/settings.py
@@ -137,7 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/d-static/'
+
+STATIC_ROOT = '/var/www/html/static'
 
 
 # Add apps/ to the Python path
