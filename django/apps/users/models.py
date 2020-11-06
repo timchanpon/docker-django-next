912c314967b6b2ffed73ead1e28ca6135b9a8cf8
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-11-06 18:37:49 +0900

    Change CustomUser's id field to uuid

--- django/apps/users/models.py
+++ django/apps/users/models.py
@@ -1,7 +1,11 @@
+import uuid
+
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
+	id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
 
 
 	class Meta:
