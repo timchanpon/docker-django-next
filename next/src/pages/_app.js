34e7b9181b732aac8aa8341dbf70540f7c8a4c55
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-11-11 20:16:28 +0900

    Create and apply app.scss

--- next/src/pages/_app.js
+++ next/src/pages/_app.js
@@ -1,7 +1,7 @@
+import '../styles/app.scss';
+
 function Root({ Component, pageProps }) {
-	return (
-		<Component {...pageProps} />
-	);
+	return <Component {...pageProps} />;
 }
 
 export default Root;
