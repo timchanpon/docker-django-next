b2339226820b8e4afd3105b68660440bb7912059
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-11-12 19:39:35 +0900

    Create login form in index page

--- next/src/pages/index.js
+++ next/src/pages/index.js
@@ -1,7 +1,37 @@
+import { useState } from 'react';
 import { connect } from 'react-redux';
 
 function Index(props) {
-	return <h1>Hello</h1>;
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	const inputHandlers = {
+		username: e => setUsername(e.target.value),
+		password: e => setPassword(e.target.value),
+	};
+
+	const fetchUserData = () => {
+		const action = {
+			type: 'users/login',
+			credentials: {
+				username: username,
+				password: password,
+			},
+		};
+
+		props.dispatch(action);
+	};
+
+	return (
+		<>
+			<input type="text" onChange={inputHandlers.username} />
+			<input type="password" onChange={inputHandlers.password} />
+			<button onClick={fetchUserData}>LOGIN</button>
+
+			<p>Your name: {props.users.name}</p>
+			<p>Your email: {props.users.email}</p>
+		</>
+	);
 }
 
 function mapStateToProps(state) {
