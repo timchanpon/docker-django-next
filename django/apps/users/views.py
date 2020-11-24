1bff5980b5fe7029fd85666197ce7e8dd0e92dab
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-11-24 23:28:28 +0900

    Modify backend login and logout functions

--- django/apps/users/views.py
+++ django/apps/users/views.py
@@ -1,18 +1,40 @@
-import os
+from datetime import datetime
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_jwt.views import ObtainJSONWebToken
+
+from django.conf import settings
 
 from .serializers import UserSerializer
+
+jwt_config = getattr(settings, 'JWT_AUTH')
+
+
+class Login(ObtainJSONWebToken):
+
+	def post(self, request, *args, **kwargs):
+		delta = jwt_config.get('JWT_EXPIRATION_DELTA')
+		expiration = datetime.utcnow() + delta
+
+		response = super().post(request)
+		response.set_cookie(
+			'isAuthenticated',
+			True,
+			expires=expiration
+		)
+
+		return response
 
 
 class Logout(APIView):
 
 	def post(self, request):
-		cookie = os.getenv('JWT_AUTH_COOKIE')
+		cookie = jwt_config.get('JWT_AUTH_COOKIE')
 
 		response = Response()
 		response.set_cookie(cookie, '', max_age=0)
+		response.set_cookie('isAuthenticated', False, max_age=0)
 
 		return response
 
@@ -32,5 +54,6 @@ class UserData(APIView):
 		return response
 
 
+login = Login.as_view()
 logout = Logout.as_view()
 user_data = UserData.as_view()
