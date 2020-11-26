cddaaaa65a0ea6438a63cee877df1cedef887fee
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-11-27 01:36:03 +0900

    Update useAuthGuard.js

--- next/src/hooks/useAuthGuard.js
+++ next/src/hooks/useAuthGuard.js
@@ -3,14 +3,10 @@ import router from 'next/router';
 
 import { isAuthCookieName, pathToLoginPage } from '../config';
 
-const dummyProps = { props: {} };
-
 function authGuardCSR() {
 	const isAuth = cookie.get(isAuthCookieName);
 
 	if (!isAuth) router.push(pathToLoginPage);
-
-	return dummyProps;
 }
 
 function authGuardSSR({ req, res }) {
@@ -19,8 +15,6 @@ function authGuardSSR({ req, res }) {
 
 	if (!isAuth)
 		res.writeHead(302, { 'Location': pathToLoginPage }).end();
-
-	return dummyProps;
 }
 
 export default function useAuthGuard(ctx) {
