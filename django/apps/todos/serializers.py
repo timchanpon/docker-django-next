993efce26d1450e0fa499fe46c59affe97570aa5
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-11-26 12:12:39 +0900

    Add create func to TodoSerializer

--- django/apps/todos/serializers.py
+++ django/apps/todos/serializers.py
@@ -8,3 +8,12 @@ class TodoSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Todo
 		exclude = ['id', 'user']
+
+	def validate(self, attrs):
+		user = self.context.get('user')
+		attrs['user'] = user
+
+		return attrs
+
+	def create(self, validated_data):
+		return Todo.objects.create(**validated_data)
