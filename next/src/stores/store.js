3e80fe84414b8ffa28cdd6761dcc758c300616fa
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-11-28 06:59:39 +0900

    Create withState HOC in store.js

--- next/src/stores/store.js
+++ next/src/stores/store.js
@@ -1,6 +1,7 @@
-import createSagaMiddleware from 'redux-saga';
+import { useSelector } from 'react-redux';
 import { createWrapper } from 'next-redux-wrapper';
 import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware, { END } from 'redux-saga';
 
 import rootSaga from './rootSaga';
 import rootReducer from './rootReducer';
@@ -18,3 +19,36 @@ function store() {
 }
 
 export const { withRedux } = createWrapper(store);
+
+export function withState(Component) {
+	const wrapper = props => {
+		props = {
+			...props,
+			...useSelector(state => state),
+		};
+
+		return <Component {...props} />;
+	};
+
+	wrapper.getInitialProps = async (ctx) => {
+		const { store } = ctx;
+		const { getInitialProps } = Component;
+		const props = getInitialProps && await getInitialProps(ctx);
+
+		/*
+		 * Await async sagaTask on server
+		 *
+		 * Reference:
+		 *  - Usage with Redux Saga
+		 *  - https://openbase.io/js/next-redux-wrapper/documentation
+		**/
+		if (!process.browser) {
+			store.dispatch(END);
+			await store.sagaTask.toPromise();
+		}
+
+		return props;
+	};
+
+	return wrapper;
+}
